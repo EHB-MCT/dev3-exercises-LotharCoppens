bc60a618802b934765bea33ad06a577e6656ae64
LotharCoppens <43748462+LotharCoppens@users.noreply.github.com>
2021-10-14 15:09:10 +0200

    gitignore

---
+++ Adventure-Game/src/main/kotlin/main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    println("Hey you, you’re finally awake. You were trying to cross the border, right? Walked right into that Imperial ambush, same as us, and that thief over there. Damn you Stormcloaks. Skyrim was fine until you came along.")
+
+
+    challengeOne()
+}
+
+fun challengeOne(){
+
+    println("Want to know more about what happen? answer my question: The one who made it doesn't need it, the one who buys it doesn't buy it for himself, and the one who needs it doesn't know he needs it.")
+
+}
+
