9495a6bff858c98fb52ecb4d741c93e4eb9af708
LotharCoppens <43748462+LotharCoppens@users.noreply.github.com>
2021-10-14 15:45:33 +0200

    WIP ChallengeThree

--- Adventure-Game/src/main/kotlin/main.kt
+++ Adventure-Game/src/main/kotlin/main.kt
@@ -9,7 +9,7 @@ fun challengeOne(){
     println("Eron: Want to know more about what happend?")
     println("Eron: answer my question: The one who made it doesn't need it, the one who buys it doesn't buy it for himself, and the one who needs it doesn't know he needs it.")
 
-    val answers = setOf<String>("Coffin", "Doodskist")
+    val answers = setOf<String>("Coffin", "coffin")
     val userAnswer = readLine()
 
     if (answers.contains(userAnswer)){
@@ -31,20 +31,43 @@ fun challengeTwo(){
     println("High King: So I heard you we're trying to cross the border, and i will let you pass it if you can tell me what my real name is")
     println("High King: I have a heart that never beats, I have a home but I never sleep. I can take a mans house and build anothers, And I love to play games with my many brothers. I am a king among fools. Who am I?")
 
-    val answersTwo = setOf<String>("King of hearts", "The King of Hearts")
+    val answersTwo = setOf<String>("King of hearts", "The King of Hearts", "king of hearts")
     val userAnswer = readLine()
 
     if (answersTwo.contains(userAnswer)){
         winTwo()
     } else {
-        gameOverTwo()
+        gameOverOne()
     }
+
+
 }
 
 fun winTwo(){
     println("Correct")
+
+    challengeThree()
 }
 
-fun gameOverTwo(){
-    println("Wrong")
+fun challengeThree(){
+    print("guess the correct dice roll to proceed")
+
+    val dice1 = (Math.random() * 6 + 1).toInt()
+    System.out.println("Roll: total = " +dice1)
+
+    val low: IntArray = intArrayOf(1, 2, 3)
+    val high: IntArray = intArrayOf(4, 5, 6)
+
+    if (dice1 == low){
+        winThree()
+    } else{
+        gameOverOne()
+    }
+
 }
+
+fun winThree(){
+    print("You guessed correctly")
+
+}
+
